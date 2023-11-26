92f821128587b8e08ad82a8785046cb8f2b7d1ca
If4x <100039513+If4x@users.noreply.github.com>
2023-11-26 13:59:06 -0700

    JS added, Typewriter added

---
+++ script.js
@@ -0,0 +1,16 @@
+// TYPEWRITER EFFECT
+var content = "My name is Imanuel. I'm a student and like to share my work.\nI'm mainly interested in photography, coding and engineering.\nOn this website you can find some of my work in these areas.\nI hope you enjoy looking at them.";
+
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+$(ele).hide().appendTo('.typewriter-text').each(function (i) {
+  $(this).delay(20 * i).css({
+    display: 'inline',
+    opacity: 0
+  }).animate({
+    opacity: 1
+  }, 100);
+});
+
+// Ensure the parent container respects line breaks
+$('.typewriter-text').parent().css('white-space', 'pre-line');
