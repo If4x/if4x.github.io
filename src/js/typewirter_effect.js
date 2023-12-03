d75edfb36151bbad8449a555ab9737cc7ff95ce7
If4x <100039513+If4x@users.noreply.github.com>
2023-12-03 00:24:41 -0700

    Changed Background Design, Added content to index

--- src/js/typewirter_effect.js
+++ src/js/typewirter_effect.js
@@ -1,20 +1,20 @@
 // TYPEWRITER EFFECT
-if (window.innerWidth > 600) {
-    var content = "My name is Imanuel. I'm a student and like to share my work.\nI'm mainly interested in photography, coding and engineering.\nOn this website you can find some of my work in these areas.\nI hope you enjoy looking at them.";
-}
-else {
-    var content = "My name is Imanuel. I'm a student and like to share my work. I'm mainly interested in photography, coding and engineering. On this website you can find some of my work in these areas. \nI hope you enjoy looking at them.";
-}
-var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
 
-$(ele).hide().appendTo('.typewriter-text').each(function (i) {
-  $(this).delay(20 * i).css({
-    display: 'inline',
-    opacity: 0
-  }).animate({
-    opacity: 1
-  }, 100);
-});
+var content = "My name is Imanuel. I'm a student and like to share my work. I'm mainly interested in photography, coding and engineering. On this website you can find some of my work in these areas.\nI hope you enjoy looking at them.";
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+var delay = 1000; //ms
+
+setTimeout(function() {
+  $(ele).hide().appendTo('.typewriter-text').each(function (i) {
+    $(this).delay(20 * i).css({
+      display: 'inline',
+      opacity: 0
+    }).animate({
+      opacity: 1
+    }, 100);
+  });
+}, delay);
+
 
 // Ensure the parent container respects line breaks
 $('.typewriter-text').parent().css('white-space', 'pre-line');
